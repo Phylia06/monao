56cb195b7b6e9002e98081c6fef16c2e6341262c
mona <nmariama45@gmail.com>
2024-09-12 17:01:18 +0100

    initial1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Dashboard from './components/Dashboard/Dashboard';
+import Landing from './components/Landing/Landing';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    
+      <div className="App">
+
+      <Dashboard />
+
+        </div>   
+     
+  );
+}
+
+export default App;
